673de523574beedeaa920a572c19f4be748a0f2f
Đàm Hữu Phước <153354626+dhphuoc@users.noreply.github.com>
2024-05-16 18:02:42 +0700

    Update tdspro5.py

--- tdspro5.py
+++ tdspro5.py
@@ -1,21 +1,16 @@
 import requests, random
 from time import sleep
-count = 0
-sonvlam = 0
-s = 0
-TOKEN = [
-     {'token':''},
-     {'token':''}, 
-     {'token':''},
-     {'token':''},
-]
-UID = [
-     {'uid':''},
-     {'uid':''}, 
-     {'uid':''},
-     {'uid':''},
-]
-tokentds = input('NHẬP TOKEN TDS: ')
+count = 0;sonvlam = 0;s = 0;dem = 0;luot = 0
+TOKEN = []
+UID = []
+tokentds = input('NHẬP ACCESS_TOKEN TDS: ')
+sopage = int(input('BẠN MUỐN CHẠY BAO NHIỀU PAGE: '))
+for i in range(sopage):
+    luot+=1
+    token = input(f"NHẬP TOKEN PAGE THỨ {luot}: ")
+    TOKEN.append({'token': token})
+    uid = input(f"NHẬP UID PAGE THỨ {luot}: ")
+    UID.append({'uid': uid})
 print('1 <> LIKE | 2 <> FOLLOW | 3 <> REACTION(COMING SOON) | 4 <> SHARE(COMING SOON) | 5 <> COMENT(COMING SOON) | 6 <> JOIN GROUP(COMING SOON) |')
 chon = input('CHỌN NHIỆM VỤ: ')
 class Api:
@@ -23,98 +18,79 @@ class Api:
         self.token = token
     def Follow (self, idnv):
         fl = requests.post(f'https://graph.facebook.com/{idnv}/subscribers?access_token={self.token}')
-        if fl.text=='true':
-            print('THEO DÕI THÀNH CÔNG!!!')
-        else:
-            print('THEO DÕI THẤT BẠI!!!')
+        if fl.text=='true':pass
     def Like(self, idnv):
         urllike = 'https://graph.facebook.com/'+str(idnv)+'/likes'
         datalike = 'access_token='+str(self.token)
         like = requests.post(urllike, data=datalike)
-        if like.text=='true':
-            print('LIKE THÀNH CÔNG!!!')
-        else:
-            print('LIKE THẤT BẠI!!!')
+        if like.text=='true':pass
     def Share(self, idnv):
         share=requests.post(f'https://graph.facebook.com/me/feed?method=POST&link=https://www.facebook.com/{idnv}&access_token={self.token}')
-        if share.json()=='true':
-            print('SHARE THÀNH CÔNG!!!')
-        else:
-            print('SHARE THẤT BẠI!!!')
-    def Reaction(self, type_post, idnv):
-        # if str(type_post) == 'LIKE':
-        #         type_rect = 'LIKE'
-        #         v = 1
-        # elif str(type_post) == 'LOVE':
-        #         type_rect = 'LOVE '
-        #         v = 2
-        # elif str(type_post) == 'CARE':
-        #         type_rect = 'CARE '
-        #         v = 3
-        # elif str(type_post) == 'HAHA':
-        #         type_rect = 'HAHA '
-        #         v = 4
-        # elif str(type_post) == 'WOW':
-        #         type_rect = 'WOW  '
-        #         v = 5
-        # elif str(type_post) == 'SAD':
-        #         type_rect = 'SAD  '
-        #         v = 6
-        # elif str(type_post) == 'ANGRY':
-        #         type_rect = 'ANGRY'
-        #         v = 7
+        if share.json()=='true':pass
+    def Reaction(self, idnv, type_post):
         reac = requests.post(f'https://graph.facebook.com/{idnv}/reactions?type={type_post}&access_token={self.token}')
-        if reac.json()=='true':
-            print(f'{type} THÀNH CÔNG!!!')
-        else:
-            print(f'{type} THẤT BẠI!!!')
+        if reac.json()=='true':pass
 def datcauhinh(uid):
     res = requests.get(f"https://traodoisub.com/api/?fields=run&id={uid}&access_token={tokentds}").json()
-    print(res)
-def nhanxu(type,idnv,tokentds):
+    if 'success' in res:
+        print(f'ĐANG CẤU HÌNH UID: {uid} | TRẠNG THÁI: THÀNH CÔNG')
+    else:
+        print(f'BẠN CHƯA THÊM UID: {uid} VÔ TDS')
+        quit()
+def nhanxu(dem,type,idnv,tokentds):
     nhan = requests.get(f'https://traodoisub.com/api/coin/?type={type}&id={idnv}&access_token={tokentds}').json()
-    print(nhan)
-while True:
-   for tokens, ids in zip(TOKEN, UID):
+    if 'success' in nhan:
+        xu = nhan['data']['xu'];idn = nhan['data']['id'];msg = nhan['data']['msg']
+        print(f'| {dem} | {idn} | {msg} | {xu} |')
+    else:
+        print(f'| X | BẠN CHƯA LÀM NHIỆM VỤ')
+if chon == '1':
+    for tokens, ids in zip(TOKEN, UID):
         token = tokens['token'].strip("\n")
         uid = ids['uid'].strip("\n")
         print(f'ĐANG CHẠY UID: {uid}')
         api = Api(token)
         datcauhinh(uid)
         while(True):
-            if chon == '1':
-                while(True):
-                    listlike=requests.get('https://traodoisub.com/api/?fields=likesieure&access_token='+str(tokentds))
-                    if 'id' in listlike.text:break
-                snv=len(listlike.json())
-                s=s+snv
-                tsnv=s
-                print(f'TÌM THẤY {str(snv)} NHIỆM VỤ LIKE')
-                for i in range(0,len(listlike.json()),1):
-                    idnv = listlike.json()[i]['id']
-                    likes = api.Like(idnv)
-                    sleep(20)
-                    type = 'LIKESIEURE'
-                    nhanx = nhanxu(type, idnv, tokentds)
-                    if likes == True:
-                        sonvlam += 1
-                    if count == 10:
-                        continue
-            elif chon == '2':
-                while(True):
-                    listfollow=requests.get(f'https://traodoisub.com/api/?fields=follow&access_token='+str(tokentds))
-                    if 'id' in listfollow.text:break
-                snv=len(listfollow.json())
-                s=s+snv
-                tsnv=s
-                print(f'TÌM THẤY {str(snv)} NHIỆM VỤ FOLLOW')
-                for i in range(0,len(listfollow.json()),1):
-                    idnv = listfollow.json()[i]['id']
-                    fl = api.Follow(idnv)
-                    sleep(50)
-                    type = 'FOLLOW'
-                    nhanx = nhanxu(type, idnv, tokentds)
-                    if fl == True:
-                        sonvlam += 1
-                    if count == 10:
-                        continue
+            listlike=requests.get('https://traodoisub.com/api/?fields=likesieure&access_token='+str(tokentds))
+            if 'id' in listlike.text:break
+        snv=len(listlike.json())
+        s=s+snv
+        tsnv=s
+        print(f'TÌM THẤY {str(snv)} NHIỆM VỤ LIKE')
+        for i in range(0,len(listlike.json()),1):
+            idnv = listlike.json()[i]['id']
+            likes = api.Like(idnv)
+            type = 'LIKESIEURE'
+            dem+=1
+            nhanx = nhanxu(dem, type, idnv, tokentds)
+            sleep(20)
+            if likes == True:
+                sonvlam += 1
+            if count == 10:
+                continue
+elif chon == '2':
+    for tokens, ids in zip(TOKEN, UID):
+        token = tokens['token'].strip("\n")
+        uid = ids['uid'].strip("\n")
+        print(f'ĐANG CHẠY UID: {uid}')
+        api = Api(token)
+        datcauhinh(uid)
+        while(True):
+            listfollow=requests.get(f'https://traodoisub.com/api/?fields=follow&access_token='+str(tokentds))
+            if 'id' in listfollow.text:break
+        snv=len(listfollow.json())
+        s=s+snv
+        tsnv=s
+        print(f'TÌM THẤY {str(snv)} NHIỆM VỤ FOLLOW')
+        for i in range(0,len(listfollow.json()),1):
+            idnv = listfollow.json()[i]['id']
+            fl = api.Follow(idnv)
+            type = 'FOLLOW'
+            dem+=1
+            nhanx = nhanxu(dem, type, idnv, tokentds)
+            sleep(20)
+            if fl == True:
+                sonvlam += 1
+            if count == 10:
+                continue
